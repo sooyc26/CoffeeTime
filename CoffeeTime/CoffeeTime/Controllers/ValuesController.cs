2bc55748a5f44a37c6f975d58c4fead13dd2b7e3
sooyong chu <sooyong.chu@sony.com>
2020-04-23 00:04:44 -0700

    jira number - check testing

--- CoffeeTime/CoffeeTime/Controllers/ValuesController.cs
+++ CoffeeTime/CoffeeTime/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace CoffeeTime.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            //test branch from master
             return new string[] { "value1", "value3" };
         }
 
